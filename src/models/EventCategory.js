9c9580920016cebb63946e4aa7b820faa01efa6d
anhdangtr <danganhanh2005@gmail.com>
2025-11-24 16:15:29 +0700

    signup login oke

---
+++ src/models/EventCategory.js
@@ -0,0 +1,9 @@
+// models/EventCategory.js
+const mongoose = require('mongoose');
+
+const eventCategorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('EventCategory', eventCategorySchema);
